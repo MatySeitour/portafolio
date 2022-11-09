328f26cefe7b2f0323ae7cfeb03943867c081b9c
Matias Seitour <matias.seitour01@gmail.com>
2022-11-09 15:20:46 -0300

    footer animation and smooth scroll

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,11 +9,23 @@ module.exports = {
             opacity: 1
           },
         },
+        jump: {
+          '0%, 100%': { transform: "translateY(0px)", },
+          '50%': {
+            transform: "translateY(10px)"
+          },
+        }
       },
       animation: {
         'wiggle': 'wiggle 1s ease-in-out',
         'wiggle2': 'wiggle 2s ease-in-out',
+        'jump1': 'jump 1s ease-in-out infinite',
+        'jump2': 'jump 2s ease-in-out infinite',
+        'jump3': 'jump 3s ease-in-out infinite',
       },
+      backgroundColor: {
+        'primary': '#0cc',
+      }
     },
   },
   content: [
@@ -23,7 +35,7 @@ module.exports = {
   daisyui: {
     themes: {
       mytheme: {
-        primary: "#7c3aed",
+        primary: "#0ff",
         secondary: "#4c1d95",
         accent: "#1FB2A6",
         neutral: "#191D24",
