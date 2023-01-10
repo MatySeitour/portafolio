3bcec44d4525d4f7aff626bc87430a1389253cdd
Matias Seitour <matias.seitour01@gmail.com>
2023-01-10 20:43:53 -0300

    resolving image style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,16 +9,18 @@ import {About} from "./About"
 function Hero(){
 
     return(
-        <header id='about_me' className="h-auto flex flex-col justify-center items-center mb-9 pt-16 pl-5 pr-5">
+        <header id='about_me' className="flex flex-col justify-center items-center mb-9 pt-40 pl-5 pr-5">
             <Nav />
-            <div className="h-screen w-full flex flex-col justify-center items-center p-5 sm:flex-row">
-                <img className="w-full h-80 animate-wiggle shadow-xl rounded-full object-cover bg-teal-300 p-2 mb-10" src={images}></img>
-                <div className="flex flex-col justify-center items-center text-center">
-                    <h1 className="text-3xl font-bold text-black mb-6 w-auto animate-wiggle tracking-widest font-serif text-shadow">MATIAS AGUSTIN SEITOUR</h1>
-                    <h2 className="mb-6 text-black text-2xl w-full font-bold animate-wiggle tracking-widest">FRONTEND DEVELOPER</h2>
-                </div>
-            <Contact />
-            </div>
+             <div className="h-auto w-full">
+                <div className="h-full w-full flex flex-col justify-center items-center p-5 sm:flex-row">
+                        <img className="w-[270px] h-[270px] animate-wiggle shadow-xl rounded-[400px] object-cover bg-teal-300 p-2 mb-10" src={images}></img>
+                        <div className="flex flex-col justify-center items-center text-center">
+                            <h1 className="text-3xl font-bold text-black mb-6 w-auto animate-wiggle tracking-widest font-serif text-shadow">MATIAS AGUSTIN SEITOUR</h1>
+                            <h2 className="mb-6 text-black text-2xl w-full font-bold animate-wiggle tracking-widest">FRONTEND DEVELOPER</h2>
+                        </div>
+                        <Contact />
+                    </div>
+             </div>
         </header>
     )
 }
