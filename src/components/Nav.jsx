4b555ad6aeb4fed7aae90ab06bc249f0a22005e5
Matias Seitour <matias.seitour01@gmail.com>
2023-01-12 00:38:00 -0300

    modifying the footer, changing the color of the background circles and some things in the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -34,12 +34,12 @@ function Nav(){
     }, [navState])
 
     return(
-        <nav className="fixed top-0 w-full z-[40] h-16 bg-slate-800">
+        <nav className="fixed top-0 w-full z-[40] max-h-[46px] bg-slate-800">
             <div className="flex relative z-[40] items-center top-0 w-full">
                 <button onClick={() => setNavState(prevState => !prevState)} className="w-20 h-16 relative z-[30] transition-all">
-                    <div className={!navState ? "h-[3px] absolute left-3 bottom-8 w-[26px] rounded-full bg-white transition-all" : "h-[3px] absolute left-3 bottom-8 w-[26px] rounded-full bg-white hamburguer-top"}></div>
-                    <div className={!navState ? "h-[3px] absolute left-3 bottom-6 w-[26px] rounded-full bg-white transition-all" : "h-[3px] absolute left-3 bottom-6 w-[26px] rounded-full bg-white hamburguer-mid"}></div>
-                    <div className={!navState ? "h-[3px] absolute left-3 bottom-4 w-[26px] rounded-full bg-white transition-all" : "h-[3px] absolute left-3 bottom-4 w-[26px] rounded-full bg-white hamburguer-bottom"}></div>
+                    <div className={!navState ? "h-[3px] absolute left-3 bottom-11 w-[26px] rounded-full bg-white transition-all" : "h-[3px] absolute left-3 bottom-8 w-[26px] rounded-full bg-white hamburguer-top"}></div>
+                    <div className={!navState ? "h-[3px] absolute left-3 bottom-9 w-[26px] rounded-full bg-white transition-all" : "h-[3px] absolute left-3 bottom-6 w-[26px] rounded-full bg-white hamburguer-mid"}></div>
+                    <div className={!navState ? "h-[3px] absolute left-3 bottom-7 w-[26px] rounded-full bg-white transition-all" : "h-[3px] absolute left-3 bottom-4 w-[26px] rounded-full bg-white hamburguer-bottom"}></div>
                 </button>
             </div>
             <div className={!navState ? "bg-black h-screen w-2/3 -translate-x-[4000px] transition-[transform]" : "pl-3 pr-3 fixed left-0 top-0 bg-black h-full w-2/3 transition-all"}>
