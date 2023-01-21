b2d2335184b60a3535339c1704c7c34ba3fbcba8
Matias Seitour <matias.seitour01@gmail.com>
2023-01-20 21:13:59 -0300

    feat(customHook) add custom hook that returns true or false depending on the resolution you give it

---
+++ src/CustomHooks/useMedia.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useLayoutEffect } from "react";
+
+function useMedia(query) {
+    const mql = window.matchMedia(query)
+    const [matches, setMatches] = useState(mql.matches)
+
+    const canUseOM = () => !!(typeof window !== "undefined" && window.document && window.document.createElement);
+
+    const useSafeLayoutEffect = canUseOM() ? useLayoutEffect : useEffect;
+
+    const handler = (e) => {
+        setMatches(mql.matches)
+    }
+
+    useSafeLayoutEffect(() => {
+        mql.addEventListener("change", handler)
+        return () => mql.removeEventListener("change", handler)
+
+    }, [])
+
+    return matches;
+}
+
+export { useMedia }
