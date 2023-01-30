ec4bcc1436b69cefc3f033ace365207c3c50a9ec
Matias Seitour <matias.seitour01@gmail.com>
2023-01-30 17:52:18 -0300

    feat(hero) add more margin to the subtitle

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ function Hero(){
                             <img className="w-[270px] h-[270px] animate-wiggle shadow-xl rounded-[400px] object-cover bg-teal-300 p-2 mb-10" src={images}></img>
                             <div className="flex flex-col justify-center items-center text-center">
                                 <h1 className="text-3xl font-bold text-black mb-6 w-auto animate-wiggle tracking-widest font-serif text-shadow">MATIAS AGUSTIN SEITOUR</h1>
-                                <h2 className="mb-6 text-black text-2xl w-full font-bold animate-wiggle tracking-widest">FRONTEND DEVELOPER</h2>
+                                <h2 className="mb-10 text-black text-2xl w-full font-bold animate-wiggle tracking-widest">FRONTEND DEVELOPER</h2>
                             </div>
                             <Contact />
                         </div>
