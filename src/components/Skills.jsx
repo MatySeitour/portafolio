6547fb0cc6eeb962fc0bb9401a0eeb25c48f4eb2
Matias Seitour <matias.seitour01@gmail.com>
2023-01-30 17:49:18 -0300

    fix(skills) fix a bug when the user scrolls to the skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -60,9 +60,9 @@ function Skills(){
     const mobileResponsive = useMedia('(max-width: 600px)');
 
     return[
-        <div className="min-h-[100vh] flex items-end" key={35}>
+        <div className="h-full flex items-end" key={35}>
             {mobileResponsive ?
-                <section ref={ref} id="skills" className="w-full h-screen overflow-hidden bg-slate-200">
+                <section ref={ref} id="skills" className="w-full h-full overflow-hidden bg-slate-200">
                     {screen && 
                         <>
                             <div className="w-full h-full flex justify-center pt-24 flex-col items-center">
